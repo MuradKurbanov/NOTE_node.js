b4b2b3c6e8c7223611d47a5eba44cc2b81adfafc
m.kurbanov <m.kurbanov@muon.team>
2020-06-03 21:20:58 +0300

    change collections

--- app/models/technologiesModel.js
+++ app/models/technologiesModel.js
@@ -2,17 +2,17 @@ const db = require('../db');
 const ObjectID = require('mongodb').ObjectID;
 
 exports.all = (cb) => {
-  db.get().collection('fruits').find().toArray((err, list) => cb(err, list))
+  db.get().collection('technologies').find().toArray((err, list) => cb(err, list))
 };
 
 exports.findById = (id, cb) => {
-  db.get().collection('fruits').findOne({'_id': ObjectID(id)}, (err, list) => cb(err, list))
+  db.get().collection('technologies').findOne({'_id': ObjectID(id)}, (err, list) => cb(err, list))
 };
 
 exports.create = (fruit, cb) => {
-  db.get().collection('fruits').insert(fruit, (err, list) => cb(err, list))
+  db.get().collection('technologies').insert(fruit, (err, list) => cb(err, list))
 };
 
 exports.delete = (id, cb) => {
-  db.get().collection('fruits').deleteOne({'_id': ObjectID(id)}, (err, list) => cb(err, list))
+  db.get().collection('technologies').deleteOne({'_id': ObjectID(id)}, (err, list) => cb(err, list))
 };
--- app/models/themesModel.js
+++ app/models/themesModel.js
@@ -1,5 +1,5 @@
-const db = require('../db');
 const ObjectID = require('mongodb').ObjectID;
+const db = require('../db');
 
 exports.all = (cb) => {
   db.get().collection('themes').find().toArray((err, list) => cb(err, list))
@@ -9,8 +9,8 @@ exports.findById = (id, cb) => {
   db.get().collection('themes').findOne({'_id': ObjectID(id)}, (err, list) => cb(err, list))
 };
 
-exports.create = (fruit, cb) => {
-  db.get().collection('themes').insert(fruit, (err, list) => cb(err, list))
+exports.create = (themes, cb) => {
+  db.get().collection('themes').insert(themes, (err, list) => cb(err, list))
 };
 
 exports.delete = (id, cb) => {

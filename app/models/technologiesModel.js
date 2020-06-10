f4e36c667bca29c394d730128a7454c0afa23ff6
m.kurbanov <m.kurbanov@muon.team>
2020-06-10 18:12:32 +0300

    added models update and delete for technologies

--- app/models/technologiesModel.js
+++ app/models/technologiesModel.js
@@ -1,27 +1,25 @@
 const db = require('../db');
-const ObjectID = require('mongodb').ObjectID;
 
 const checkResponse = (res, err, data) => {
-  if (err) res.sendStatus(500);
-  else res.send(data)
-};
-
-exports.all = (res) => {
-  db.get().collection('technologies').find().toArray((err, list) => checkResponse(res, err, list))
-};
-
-exports.findOne = (id, res) => {
-  db.get().collection('technologies').findOne({'_id': ObjectID(id)}, (err, list) => checkResponse(res, err, list))
+  if (err) {
+    console.log(err);
+    return res.sendStatus(500);
+  } else res.send(data)
 };
 
 exports.create = (technology, res) => {
   db.get().collection('technologies').insertOne(technology, (err, list) => checkResponse(res, err, list))
 };
 
-exports.delete = (id, res) => {
-  db.get().collection('technologies').deleteOne({'_id': ObjectID(id)}, (err, list) => checkResponse(res, err, list))
+exports.find = (params, res) => {
+  db.get().collection('technologies').find(params).toArray((err, data) => checkResponse(res, err, data))
 };
 
-exports.deleteAll = res => {
-  db.get().collection('technologies').deleteMany({}, (err, list) => checkResponse(res, err, list))
+exports.update = (id, technology, res) => {
+  db.get().collection('technologies').updateOne(id, { $set: technology }, { upsert: false },
+    (err, data) => checkResponse(res, err, data));
+};
+
+exports.delete = (id, res) => {
+  db.get().collection('technologies').deleteOne(id, (err, list) => checkResponse(res, err, list))
 };
--- app/models/themesModel.js
+++ app/models/themesModel.js
@@ -8,23 +8,19 @@ const checkResponse = (res, err, data) => {
   else res.send(data)
 };
 
-// Create
 exports.create = (theme, res) => {
   db.get().collection('themes').insertOne(theme, (err, data) => checkResponse(res, err, data))
 };
 
-// Read
 exports.find = (params, res) => {
   db.get().collection('themes').find(params).toArray((err, data) => checkResponse(res, err, data))
 };
 
-// Update
 exports.update = (id, theme, res) => {
   db.get().collection('themes').updateOne(id, { $set: theme }, { upsert: false },
     (err, data) => checkResponse(res, err, data));
 };
 
-// Delete
 exports.delete = (id, res) => {
   db.get().collection('themes').deleteOne(id, (err, data) => checkResponse(res, err, data))
 };

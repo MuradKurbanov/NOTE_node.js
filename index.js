6d63dbebc48f0c942a6a26ecd9ff31a995c1e1fe
m.kurbanov <m.kurbanov@muon.team>
2020-06-16 20:50:43 +0300

    connect server-mongodb and update gitignore

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const db = require('./app/db');
 const router = require('./app/routes/routes');
+const dbUrl = require('./dbUrl');
 
 const app = express();
 
-db.connect('mongodb://localhost:27017', err => {
+db.connect(dbUrl, err => {
   if (err) console.log(err);
   else app.use('/', router).listen(3000);
 });
